a2e7ca3852144b6b127067a4dcdc8687514ad1a9
Aniket7726 <aniketsharma.sh17@gmail.com>
2024-09-08 12:04:01 +0530

    first commit

---
+++ foot.php
@@ -0,0 +1,26 @@
+<div class="foot">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="col-box">
+                    
+                </div>                
+            </div>
+            <div class="col-lg-3">
+                <div class="col-box">
+                    
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="col-box">
+                    
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="col-box">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ navbar.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg p-0">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="Images/navbar-logo.png" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" style="background-color:#fff;" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item px-3">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Projects</a>
+        </li>
+        <li class="nav-item dropdown px-3">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Projects
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">FAQ's</a>
+        </li>
+        <li class="nav-item px-3">
+          <a class="nav-link" href="#">Events</a>
+        </li>
+      </ul>
+    </div>
+    <div class="d-none d-sm-none d-md-none d-lg-flex d-xl-flex">
+        <button>
+            Contact Us 
+            <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37" fill="none">
+            <circle cx="18.5" cy="18.5" r="18.0375" stroke="white" stroke-width="0.925"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.3791 12.2959L22.2657 18.1826C22.627 18.5438 22.627 19.1295 22.2657 19.4907L16.3791 25.3774C16.0178 25.7386 15.4321 25.7386 15.0709 25.3774C14.7097 25.0162 14.7097 24.4305 15.0709 24.0692L20.3035 18.8367L15.0709 13.6041C14.7097 13.2428 14.7097 12.6572 15.0709 12.2959C15.4321 11.9347 16.0178 11.9347 16.3791 12.2959Z" fill="white"/>
+            </svg>
+        </button>
+    </div>
+  </div>
+</nav>
